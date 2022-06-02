3627697a7c7db8a3d891a28c7397ce45439dfcc1
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-02 14:03:19 -0700

    WIP service and UI scripts for API call and display

--- src/index.js
+++ src/index.js
@@ -8,5 +8,21 @@ import {CurrencyService} from './services/currency-service.js';
 
 // JQUERY UI Logic
 $(document).ready(function() {
- 
+  $('#calculate').click(function() {
+    let currency1 = "USD";
+    let currency2 = "EUR";
+    let amount = $('#amount').val();
+    CurrencyService.getExchange(currency1, currency2, amount)
+      .then(function(exchangeResponse) {
+        console.log(exchangeResponse);
+        if (exchangeResponse instanceof Error) {
+          throw Error(`API error: ${exchangeResponse.message}`);
+        }
+        const exchange = exchangeResponse.conversion_result;
+        $('.show-exchange').text(`The exchange is ${exchange} ${currency2}.`);
+      })
+      .catch(function(error) {
+        $('.show-errors').text(`${error}`);
+      });
+  });
 });
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,15 +1,15 @@
 export class CurrencyService {
-  static getCurrency(currency1, currency2, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/pair/${currency1}/${currency2}/${amount}`)
-    .then(function(response) {
-      if (!response.ok)
-      {
-        throw Error(response.error-type)
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    })
+  static getExchange(currency1, currency2, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
+      .then(function(response) {
+        if (!response.ok)
+        {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
   }
 }
