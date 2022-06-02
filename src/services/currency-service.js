68dda1d191a1fa0df214437fc40a09c6bf0c0929
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-02 14:20:30 -0700

    complete API call scripts and add dotenv plugin

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,7 +1,10 @@
 export class CurrencyService {
   static getExchange(currency1, currency2, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
+    let url = (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`);
+    console.log(url);
+    return fetch(url)
       .then(function(response) {
+        console.log(process.env.API_KEY);
         if (!response.ok)
         {
           throw Error(response.statusText);
