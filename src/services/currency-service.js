0f4044e16cad43b00b62a1f70b851476d2e82dc0
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-02 20:53:21 -0700

    add input/select html and more currencies

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,10 +1,7 @@
 export class CurrencyService {
   static getExchange(currency1, currency2, amount) {
-    let url = (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`);
-    console.log(url);
-    return fetch(url)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
       .then(function(response) {
-        console.log(process.env.API_KEY);
         if (!response.ok)
         {
           throw Error(response.statusText);
