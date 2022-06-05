e84e35a3acda267f9f1d5747003fc053db0f3cd5
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-05 09:01:12 -0700

    remove console.logs and correct formatting

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {CurrencyService} from './services/currency-service.js';
+import { CurrencyService } from './services/currency-service.js';
 
 // UI Logic
 function populateSelect(response) {
   const currencyCodes = response.supported_codes;
   $('#select').empty();
-  currencyCodes.forEach(function(code) {
+  currencyCodes.forEach(function (code) {
     $('#currency-from').append($('<option></option>').val(code[0]).html(code[0] + " - " + code[1]));
   });
-  currencyCodes.forEach(function(code) {
+  currencyCodes.forEach(function (code) {
     $('#currency-to').append($('<option></option>').val(code[0]).html(code[0] + " - " + code[1]));
   });
 }
 
 function displayExchange(exchangeResponse) {
-  console.log(exchangeResponse);
   const exchange = exchangeResponse.conversion_result;
   $('#to-amount').val(exchange);
   $('#amountCalculated').text(exchange);
@@ -33,13 +32,13 @@ function displayErrors(error) {
 }
 
 // JQUERY UI Logic
-$(document).ready(function() {
+$(document).ready(function () {
   CurrencyService.getCurrencyList()
-    .then(function(response) {
+    .then(function (response) {
       populateSelect(response);
     });
 
-  $('#calculateForm').submit(function(event) {
+  $('#calculateForm').submit(function (event) {
     event.preventDefault();
 
     let currency1 = $('#currency-from').val();
@@ -48,21 +47,18 @@ $(document).ready(function() {
     $('#amountEntered').text(amount);
 
     CurrencyService.getExchange(currency1, currency2, amount)
-      .then(function(exchangeResponse) {
-        console.log(exchangeResponse);
+      .then(function (exchangeResponse) {
         if (exchangeResponse["error-type"] === "unsupported-code") {
           throw Error("currency code does not exist");
         } else if (exchangeResponse.result === "success" && exchangeResponse.conversion_result === undefined) {
           throw Error(`Input amount must be greater than or equal to 0.01`);
-        } else if (exchangeResponse instanceof Error) { 
+        } else if (exchangeResponse instanceof Error) {
           throw Error(`Exchange Rate API ${exchangeResponse}`);
         }
-        
-        console.log(exchangeResponse);
+
         displayExchange(exchangeResponse);
       })
-      .catch(function(error) {
-        console.log(error.message);
+      .catch(function (error) {
         displayErrors(error.message);
       });
   });
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,27 +1,26 @@
 export class CurrencyService {
   static getCurrencyList() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
-      .then(function(response) {
+      .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
         return response.json();
       })
-      .catch(function(error) {
+      .catch(function (error) {
         return error;
       });
   }
 
   static getExchange(currency1, currency2, amount) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
-      .then(function(response) {
-        console.log(response);
+      .then(function (response) {
         if (!response.ok && response.status != 404) {
           throw Error(response.statusText);
         }
         return response.json();
       })
-      .catch(function(error) {
+      .catch(function (error) {
         return Error(error);
       });
   }
